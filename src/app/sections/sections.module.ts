c7f9cfcc4bada66f1561d28eeb397b2c9c17e280
mazenmdimegh <mazenmdimagh@gmail.com>
2022-02-15 10:50:55 +0100

    error fixed

--- src/app/sections/sections.module.ts
+++ src/app/sections/sections.module.ts
@@ -11,10 +11,8 @@ import { ButtonsSectionComponent } from './buttons-section/buttons-section.compo
 import { InputsSectionComponent } from './inputs-section/inputs-section.component';
 import { CrsSectionComponent } from './crs-section/crs-section.component';
 import { NavigationSectionComponent } from './navigation-section/navigation-section.component';
-import { TabsSectionComponent } from './tabs-section/tabs-section.component';
 import { AlertsSectionComponent } from './alerts-section/alerts-section.component';
 import { TypographySectionComponent } from './typography-section/typography-section.component';
-import { AngularSectionComponent } from './angular-section/angular-section.component';
 import { NucleoSectionComponent } from './nucleo-section/nucleo-section.component';
 import { VersionsSectionComponent } from './versions-section/versions-section.component';
 import { NgbdModalComponent } from './modal/modal.component';
@@ -27,10 +25,8 @@ import { NgbdModalContent } from './modal/modal.component';
     InputsSectionComponent,
     CrsSectionComponent,
     NavigationSectionComponent,
-    TabsSectionComponent,
     AlertsSectionComponent,
     TypographySectionComponent,
-    AngularSectionComponent,
     NucleoSectionComponent,
     VersionsSectionComponent,
     NgbdModalComponent,
