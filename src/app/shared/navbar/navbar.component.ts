24499108433aab5f6d6c37a3ecc3438712729a06
mazenmdimegh <mazenmdimagh@gmail.com>
2022-04-06 16:56:45 +0100

    Integration

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -11,7 +11,7 @@ export class NavbarComponent implements OnInit {
     public isCollapsed = true;
     private lastPoppedUrl: string;
     private yScrollStack: number[] = [];
-
+    public LoggedIn = false;
     constructor(public location: Location, private router: Router) {
     }
 
